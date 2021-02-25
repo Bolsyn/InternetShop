30a8734d81b289e1ab216b3c9f308ed0073bf4a4
Bolsyn <Serikagis@yandex.ru>
2021-02-25 21:55:39 +0600

    Added smsService and SingUpMenu(UnReady)

---
+++ InternetShop/SignUpMenu.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace InternetShop
+{
+    public class SignUpMenu
+    {
+        public void PrintSignUp()
+        {
+            Console.WriteLine("Регистрация? Нажмите 1");
+            Console.WriteLine("Вход? Нажмите 2");
+            Console.WriteLine("Выход? Нажмите 0");
+            int signUp = Console.Read() - 48;
+
+            var rand = new Random();
+            int code = rand.Next() % 89999 + 10000;
+            if (signUp == 1)
+            {
+                Console.WriteLine("Введите номер, (+XYYYZZZZZZZ)");
+                Console.Write("где, X - код страны, Y - код опрератора, Z - номер телефона: ");
+                Console.ReadLine();
+                string phone = Console.ReadLine();
+
+                Twillio.SMSService(phone, code);
+
+                Console.Write("Напиште код который пришел на ваш телефон: ");
+                string inputCode = Console.ReadLine();
+
+                if (Int32.TryParse(inputCode, out int intCode))
+                {
+                    intCode = Int32.Parse(inputCode);
+                }
+
+                if (intCode == code)
+                {
+                    Console.WriteLine("Поздравляю, вы зарегистрировались!");
+                }
+                else
+                {
+                    Console.WriteLine("Заново!");
+                }
+            }
+            if (signUp == 2)
+            {
+                Console.WriteLine("Введите номер, (+XYYYZZZZZZZ)");
+                Console.Write("где, X - код страны, Y - код опрератора, Z - номер телефона: ");
+                Console.ReadLine();
+                string phone = Console.ReadLine();
+
+                Twillio.SMSService(phone, code);
+
+                Console.Write("Напиште код который пришел на ваш телефон: ");
+                string inputCode = Console.ReadLine();
+
+                if (Int32.TryParse(inputCode, out int intCode))
+                {
+                    intCode = Int32.Parse(inputCode);
+                }
+
+                if (intCode == code)
+                {
+                    Console.WriteLine("Поздравляю, вы зарегистрировались!");
+                }
+                else
+                {
+                    Console.WriteLine("Заново!");
+                }
+            }
+            else if (signUp == 0)
+            {
+                Console.WriteLine("До свидания!");
+            }
+        }
+    }
+}
---
+++ InternetShop/Twillio.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace InternetShop
+{
+    public class Twillio
+    {
+        public static void SMSService(string phone, int code)
+        {
+            SendSms(phone, code);
+            Console.Write("Press any key to continue.");
+            Console.ReadKey();
+        }
+
+        static void SendSms(string phone, int code)
+        {
+            string accountSid = Environment.GetEnvironmentVariable("AC297c271ef56c987b77ec37e2d3da4206");
+            string authToken = Environment.GetEnvironmentVariable("9ae12edd18e44255eacd5c46bd5d5bf1");
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: $"Your code is {code}",
+                from: new Twilio.Types.PhoneNumber("+12059472980"),
+                to: new Twilio.Types.PhoneNumber(phone)
+            );
+
+            Console.WriteLine(message.Sid);
+        }
+    }
+}
