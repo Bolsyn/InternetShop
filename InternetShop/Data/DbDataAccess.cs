f4b11becfa57f5a81c02f4c8a446a975cddbb06c
Anuar <beisekeyev.a.1992.ast@gmail.com>
2021-03-01 17:15:17 +0600

    Added the DbDataAccess class

---
+++ InternetShop/Data/DbDataAccess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Common;
+using InternetShop.Services;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+
+namespace InternetShop.Data
+{
+    public abstract class DbDataAccess<T> : IDisposable
+    {
+        protected readonly DbProviderFactory factory;
+        protected readonly DbConnection connection;
+
+        public DbDataAccess()
+        {
+            factory = DbProviderFactories.GetFactory("ConnectedProvider");
+            connection = factory.CreateConnection();
+            connection.ConnectionString = ConfigurationService.Configuration["DataAccessConnectionString"];
+            connection.Open();
+        }
+        public void Dispose()
+        {
+            connection.Close();
+        }
+    }
+}
---
+++ InternetShop/Services/ConfigurationService.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlClient;
+using System.Data.Common;
+using Microsoft.Extensions.Configuration;
+
+
+namespace InternetShop.Services
+{
+    public static class ConfigurationService
+    {
+        public static IConfiguration Configuration { get; private set; }
+
+        public static void Init()
+        {
+            if(DbProviderFactories.GetFactory("ConnectedProvider") == null)
+            {
+                DbProviderFactories.RegisterFactory("ConnectedProvider", SqlClientFactory.Instance);
+            }
+            if(Configuration == null)
+            {
+                var configuratonBuilder = new ConfigurationBuilder();
+                var configuration = configuratonBuilder.AddJsonFile("appSetting.json").Build();
+            }
+        }
+    }
+}
